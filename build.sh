d4a30f25968ac71cdced415125deaad958638077
mande-ship-it <mande@ageafrica.org>
2026-08-05 20:37:49 +0200

    pushing to git hub

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 1. Clone Flutter stable branch if not exists
+# 1. Clone Flutter stable branch
 if [ ! -d "flutter" ]; then
   echo "Cloning Flutter SDK..."
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
@@ -9,14 +9,15 @@ fi
 # 2. Add Flutter to the PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 3. Check Flutter version (This also triggers tool download)
+# 3. Trigger tool download and check version
 flutter --version
 
-# 4. Precache web artifacts
-flutter precache --web
-
-# 5. Enable Web support
+# 4. Enable Web support
 flutter config --enable-web
+
+# 5. Fetch dependencies
+echo "Fetching dependencies..."
+flutter pub get
 
 # 6. Build the web project
 echo "Building Web App..."
